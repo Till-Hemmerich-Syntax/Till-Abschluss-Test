fe9a5375d27a89c6b42d4eca8a9908e2bcf90ae8
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-06-23 11:16:01 +0200

    Initial Commit

---
+++ Till-Abschluss/Till_AbschlussApp.swift
@@ -0,0 +1,17 @@
+//
+//  Till_AbschlussApp.swift
+//  Till-Abschluss
+//
+//  Created by Till Hemmerich on 23.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Till_AbschlussApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
